98333f910df84c8c2049ed21be1899890d4b5273
lempute3 <linuxlinux742@gmail.com>
2023-04-06 00:14:02 +0300

    Update AppCredentials.java

--- app/src/main/java/com/example/app/utils/AppCredentials.java
+++ app/src/main/java/com/example/app/utils/AppCredentials.java
@@ -3,11 +3,11 @@ package com.example.app.utils;
 public class AppCredentials {
 
     /*Firebase Credentials*/
-    public static final String FIREBASE_DB_URL = " https://flickframe-c767b-default-rtdb.europe-west1.firebasedatabase.app";
+    public static final String FIREBASE_DB_URL = "....";
 
     /*TMDB Credentials*/
     public static final String TMDB_BASE_URL = "https://api.themoviedb.org/";
     public static final String TMDB_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
-    public static final String TMDB_API_KEY = "df15ab683ea7554e6bc60a266bf6da1f";
+    public static final String TMDB_API_KEY = "....";
 
 }
